f6df5d15168fe12d311af46a1c01c464bd9028f9
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-03-05 23:38:50 +0000

    feat: make errors more detailed

--- index.js
+++ index.js
@@ -53,14 +53,14 @@ client.trySend = async (channelID, guild, message, errorMessage) => {
 
 process.on('unhandledRejection', async error => {
   console.log(error);
-  const lista = await new discord.AttachmentBuilder(Buffer.from(error), {
+  const lista = await new discord.AttachmentBuilder(error, {
     name: 'unhandledRejection.txt',
   });
   client.channels.cache.get(client.canais.errors).send({ files: [lista] });
 });
 process.on('uncaughtException', async error => {
   console.log(error);
-  const lista = await new discord.AttachmentBuilder(Buffer.from(error), {
+  const lista = await new discord.AttachmentBuilder(error, {
     name: 'uncaughtException.txt',
   });
   client.channels.cache.get(client.canais.errors).send({ files: [lista] });
