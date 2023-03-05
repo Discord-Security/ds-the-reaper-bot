e719de214849ed2b77e661328ac18354caad6565
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-03-05 18:13:24 +0000

    feat: NQN function

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -2,6 +2,7 @@ const discord = require('discord.js');
 
 module.exports = async (client, message) => {
   if (message.guild === null) return;
+
   const doc = await client.db.Guilds.findOne({ _id: message.guild.id });
 
   if (doc) {
@@ -146,5 +147,55 @@ module.exports = async (client, message) => {
     }
   } else if (!doc) {
     return new client.db.Guilds({ _id: message.guild.id }).save();
+  }
+
+  let msg = message.content;
+
+  const emojis = msg.match(/(?<=:)([^:\s]+)(?=:)/g);
+  if (!emojis) return;
+  emojis.forEach(m => {
+    const emoji = client.emojis.cache.find(x => x.name === m);
+    if (!emoji) return;
+    const temp = emoji.toString();
+    if (new RegExp(temp, 'g').test(msg))
+      msg = msg.replace(new RegExp(temp, 'g'), emoji.toString());
+    else msg = msg.replace(new RegExp(':' + m + ':', 'g'), emoji.toString());
+  });
+
+  if (msg !== message.content) {
+    let webhook = await message.channel.fetchWebhooks();
+    const number = randomNumber(1, 2);
+    function randomNumber(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    webhook = webhook.find(x => x.name === 'NQN' + number);
+
+    if (!webhook) {
+      webhook = await message.channel.createWebhook({
+        name: `NQN` + number,
+        avatar: client.user.displayAvatarURL({ dynamic: true }),
+      });
+    }
+
+    await webhook.edit({
+      name: message.member.nickname
+        ? message.member.nickname
+        : message.author.username,
+      avatar: message.author.displayAvatarURL({ dynamic: true }),
+    });
+
+    message.delete().catch(() => {
+      return 0;
+    });
+    webhook.send(msg).catch(() => {
+      return 0;
+    });
+
+    await webhook.edit({
+      name: `NQN` + number,
+      avatar: client.user.displayAvatarURL({ dynamic: true }),
+    });
   }
 };
