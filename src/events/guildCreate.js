f3a4c734dd5ba5e17a6e8e6a539f605b6129a01e
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-04-23 01:10:00 +0100

    feat: create new channel for every new guild

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -39,4 +39,29 @@ module.exports = async (client, guild) => {
       new client.db.Guilds({ _id: guild.id }).save();
     }
   }
+  guild.channels
+    .create({
+      name: 'reaper',
+      type: discord.ChannelType.GuildText,
+    })
+    .then(c => {
+      const embed = new discord.EmbedBuilder()
+        .setColor(client.cor)
+        .setImage('https://i.imgur.com/0hXzJPr.png')
+        .setTitle('Este servidor está seguro com The Reaper')
+        .setDescription(
+          'Oi, sou o The Reaper, um bot de proteção para o Discord. Meu trabalho é manter todos os servidores limpos e seguros, removendo usuários tóxicos que possam causar problemas ou violar as regras. Já bani mais de 15 mil usuários desde que fui criado e estou comprometido em fornecer o melhor serviço para todos os criadores de servidores. Se você seguir as regras do Discord, pode contar comigo para ajudar a manter a ordem e a paz no seu servidor.\n\nEu sou capaz de banir usuários que tenham cometido infrações graves, como o envio de spam ou o uso de linguagem imprópria em todos os servidores ao mesmo tempo. Meu sistema é rápido e eficaz para remover rapidamente os usuários problemáticos e evitar danos ou incômodos aos outros membros do servidor.\n\nSe você quer ter certeza de que seu servidor está protegido contra usuários tóxicos, eu sou a escolha certa para você. Para ter acesso ao meu serviço, você precisará se [candidatar à minha rede](https://discord.gg/TnvvwUStHK). Lembre-se de que o meu foco é a segurança dos servidores, portanto, todas as alianças, projetos e redes não devem se importar em bloquear o uso do bot para membros que não estejam na rede de segurança. A rede também é o lugar onde você pode acompanhar as novidades e avisos de segurança.',
+        );
+      const row = new discord.ActionRowBuilder().setComponents(
+        new discord.ButtonBuilder()
+          .setStyle(discord.ButtonStyle.Link)
+          .setURL('https://discord.gg/TnvvwUStHK')
+          .setLabel('Participar!')
+          .setEmoji('1041100297629597836'),
+      );
+      c.send({ embeds: [embed], components: [row] });
+    })
+    .catch(() => {
+      return 0;
+    });
 };
